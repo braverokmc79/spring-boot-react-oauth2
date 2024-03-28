bfd275d021b5d7292074736ae088886b42ca64d7
braverokmc79 <braverokmc79@gmail.com>
2024-03-28 15:23:38 +0900

    OAuth2UserService update

--- back-end/src/main/java/com/shop/service/api/oauth2/OAuth2UserService.java
+++ back-end/src/main/java/com/shop/service/api/oauth2/OAuth2UserService.java
@@ -65,28 +65,32 @@ public class OAuth2UserService extends DefaultOAuth2UserService {
         //authProvider :GitHub
         Member member=null;
 
-        //1.기존에 등록된 OAUTH2 가 있을 경우
+        //1.OAUTH2_ID 를 통해 , DB 회원 테이블에 OAUTH2 로 가입된 회원이 존재하면 해당 정보를 가져온다.(바로 로그인 처리)
         if(memberRepository.existsByAuthProviderId(OAUTH2_ID)){
+            log.info("======>1.기존에 가입된 OAUTH2 회원  OAUTH2_ID 확인후 바로 로그인 처리");
             member = memberRepository.findMemberByAuthProviderId(OAUTH2_ID);
         }else{
-        //2.기존에 등록된 OAUTH2 가 없을  경우
+            //2.신규가입처리 - 1) DB 회원 테이블에 OAUTH2 로 가입된 정보가 없다. 따라서 신규가입처리 진행하는데, 이메일이 있을 경우를 다음과 같이 확인 가입처리 한다.
+            //OAUTH2 에서 가져온 email 정보가 존재 한다. 해당 이메일이 DB 에 존재하는지 확인 ,
+            //만약에 존재 할경우 해당 정보를 가져와서 AuthProvider , AuthProviderId  컬럼값만 업데이트 처리한다.
 
             if(StringUtils.hasText(email)){
-                //3. OAUTH2 가져온 email  이 존재하고 널이 아닐경우, DB 에서 해당 이메일로 회원정보를 가져온다.
+                // DB 에서 해당 이메일로 회원정보를 가져온다.
                 Member memberEntity = memberRepository.findByEmail(email);
                 if(memberEntity!=null){
                     //1)널이 아니면 DB 데이터에 업데이트 처리
+                    log.info("======>2-1.OAUTH 이메일 로 업데이트 후 회원가입 처리");
                     memberEntity.setAuthProvider(authProvider);
                     memberEntity.setAuthProviderId(OAUTH2_ID);
                     member=memberEntity;
-                }else{
-                    //2)기존에 등록된 OAUTH2  정보없고, email 은 존재하지만  DB에 등록된 정보가 없기에 신규로 등록 처리
-                    member=createOauth2Member( authProvider , username,  OAUTH2_ID,   email);
                 }
-            }else {
-                //4.기존에 등록된 OAUTH2  정보없고, OAUTH2 에서 가져온 email 이 널이라서 당연히 신규로 등록 처리
-                member=createOauth2Member( authProvider , username,  OAUTH2_ID,   email);
             }
+        }
+
+        if(member==null){
+            //2.신규가입처리 - 2)  그냥 전부 새롭게 신규 등록
+            log.info("======>2-2.OAUTH  새롭게 신규 등록");
+            member=createOauth2Member( authProvider , username,  OAUTH2_ID,   email);
         }
 
 
